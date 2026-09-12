605c6882f402a7437bb34afd5578e873607c961a
system-99 publisher <publisher@99point.co>
2026-09-12 22:29:27 +0000

    Pin install.sh to 62b0ddea8f6a8be806f81be2bdbc6919caab0b91

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@
 set +x
 set -euo pipefail
 
-RELEASE_COMMIT='3feeb31b6f73d3b1c7db32f484a2c040aa09cb3e'
-GENESIS_SHA256='a514fe0174818e913764ead4f2fcb328cf557d073c76a359d7be5f155bc120e3'
+RELEASE_COMMIT='62b0ddea8f6a8be806f81be2bdbc6919caab0b91'
+GENESIS_SHA256='91df2887928b07dbc3ad18cfd48e15a92e89df7404f352e5de53b88ef618ef40'
 SETUP_SHA256='d764ae1f997956252d07ee0431a967e3b2025eee8785a1eef9b6bd8501e10798'
 # GENESIS_SOURCE overrides the download base for mirrors and local checks;
 # GENESIS_INSTALL_URL is the publisher `genesis update` re-fetches this script from.
